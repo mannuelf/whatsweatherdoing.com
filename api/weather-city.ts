4066f55e7a432bf4ed2806efff157afb2867904d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-08-01 16:16:49 +0200

    fix city endpoint to return 200 respons

--- api/weather-city.ts
+++ api/weather-city.ts
@@ -1,6 +1,6 @@
 import { ServerRequest } from "https://deno.land/std@0.89.0/http/server.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 oakCors();
 config();
@@ -10,31 +10,29 @@ export default async (req: ServerRequest): Promise<Response> => {
 	const apiKey = Deno.env.get("OPEN_WEATHER_API_KEY");
 	let _jsonData;
 
-	try {
-		const params = new URLSearchParams(req.url);
-		for (const param of params) {
-			const url = new URL(
-				`${baseUrl}?q=${param[1]}&appid=${apiKey}&units=metric`
-			);
+	const params = new URLSearchParams(req.url);
+	for (const param of params) {
+		const url = new URL(
+			`${baseUrl}?q=${param[1]}&appid=${apiKey}&units=metric`
+		);
 
-			const response = await fetch(url);
-			const jsonData = await response.json();
-			_jsonData = jsonData;
-		}
-
-		req.headers.append("Access-Control-Allow-Origin", "*");
-		req.headers.append("Access-Control-Allow-Methods", "GET");
-
-		return new Response(JSON.stringify(_jsonData), {
-			status: 200,
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Methods": "GET",
-				"content-type": "application/json; charset=utf-8",
-			},
-		});
-	} catch (error) {
-		console.log(`Something went wrong: ${error}`);
-		return new Response(`Something went wrong: ${error}`);
+		const response = await fetch(url);
+		const jsonData = await response.json();
+		_jsonData = jsonData;
 	}
+
+	req.headers.append("Access-Control-Allow-Origin", "*");
+	req.headers.append("Access-Control-Allow-Methods", "GET");
+	req.respond({
+		body: JSON.stringify(_jsonData),
+		status: 200,
+	});
+	return new Response(JSON.stringify(_jsonData), {
+		status: 200,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET",
+			"content-type": "application/json; charset=utf-8",
+		},
+	});
 };
