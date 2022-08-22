fbee93c19ebec7f2c194186bdf11b3b005e1abef
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-08-22 21:11:37 +0200

    fixing cors issues

--- api/weather-city.ts
+++ api/weather-city.ts
@@ -20,7 +20,19 @@ export default async (req: ServerRequest): Promise<Response> => {
 	}
 
 	console.log("🏳 req.headers", req.headers);
-
+	req.headers.append("Access-Control-Allow-Origin", "*");
+	req.headers.append(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	req.headers.append("Access-Control-Allow-Credentials", "true");
+	req.headers.append("Access-Control-Allow-Methods", "GET");
+	//Preflight CORS handler
+	if (req.method === "OPTIONS") {
+		return new Response(JSON.stringify({ body: "OK" }), {
+			status: 200,
+		});
+	}
 	return new Response(JSON.stringify(_jsonData), {
 		status: 200,
 		headers: {
--- api/weather.ts
+++ api/weather.ts
@@ -19,7 +19,20 @@ export default async (req: ServerRequest): Promise<Response> => {
 	const response = await fetch(url);
 	const jsonData = await response.json();
 
+	req.headers.append("Access-Control-Allow-Origin", "*");
+	req.headers.append(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	req.headers.append("Access-Control-Allow-Credentials", "true");
+	req.headers.append("Access-Control-Allow-Methods", "GET");
 	console.log("🏳 req.headers", req.headers);
+	// Preflight CORS handler
+	if (req.method === "OPTIONS") {
+		return new Response(JSON.stringify({ body: "OK" }), {
+			status: 200,
+		});
+	}
 
 	return new Response(JSON.stringify(jsonData), {
 		status: 200,
